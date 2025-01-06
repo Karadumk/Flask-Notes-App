54743308714a51477d6ba4caeba606a09a94a798
Karadumk <karadumk223@gmail.com>
2025-01-06 15:31:33 +0100

    Add main.py as program entry point

---
+++ main.py
@@ -0,0 +1,7 @@
+from website import create_app
+
+app = create_app()  # initializes the flask app
+
+# ensures the server only runs when the script is executed directly
+if __name__ == "__main__":
+    app.run(debug=True)
